1c39856d8e5fe192638542831231c22a0c4085f9
zhouyang <zhouyang2021@163.com>
2023-11-27 19:06:57 +0800

    add files

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  // 添加格式化规则
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        semi: false,
+        wrapAttributes: false,
+        printWidth: 100,
+        endOfLine: 'auto'
+      }
+    ]
+  }
+}
